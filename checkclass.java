6dae4184e02f602880a383af5b23a75b174e3936
Kartik Raiyani <93818535+kartikraiyani03@users.noreply.github.com>
2023-09-19 19:50:21 +0530

    Add files via upload

---
+++ checkclass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ 
+public class checkclass
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> sr = Array.asList(1,2,3,4,5);
+
+    }
+}
---
+++ clone.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ 
+public class clone
+{
+    public static void main(String args[])
+    {
+        // System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a");
+        int a = sc.nextInt();
+
+        System.out.println("Enter b");
+        int b = sc.nextInt();
+
+        int c;
+
+        c = a+b;
+        
+        System.out.println("Sum is : "+c);
+
+    }
+}
